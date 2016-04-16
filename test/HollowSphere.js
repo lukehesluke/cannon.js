7a72b1328ae76641b85fcf0e3ba11311cd4c58c3
Luke Winship <luke.winship@googlemail.com>
2016-04-16 18:34:42 +0100

    Now adhering to semicolon style in test

--- test/HollowSphere.js
+++ test/HollowSphere.js
@@ -5,28 +5,31 @@ var almostEqual = function(numA, numB, precision) {
     if(precision===undefined){
         precision = 1e-6;
     }
-    return Math.abs(numA - numB) < precision
+    return Math.abs(numA - numB) < precision;
 }
 
 module.exports = {
     volume: function(test) {
-        test.ok(almostEqual(new HollowSphere(10).volume(), 0))
-        test.ok(almostEqual(new HollowSphere(3).volume(), 0))
+        test.ok(almostEqual(new HollowSphere(10).volume(), 0));
+        test.ok(almostEqual(new HollowSphere(3).volume(), 0));
+        test.done();
     },
     updateBoundingSphereRadius: function(test) {
-        test.equal(new HollowSphere(10).updateBoundingSphereRadius(), 10)
-        test.equal(new HollowSphere(3).updateBoundingSphereRadius(), 3)
+        test.equal(new HollowSphere(10).updateBoundingSphereRadius(), 10);
+        test.equal(new HollowSphere(3).updateBoundingSphereRadius(), 3);
+        test.done();
     },
     calculateLocalInertia: function(test) {
         test.ok(
             new Vec3(333.3333333333333, 333.3333333333333, 333.3333333333333).almostEquals(
                 new HollowSphere(10).calculateLocalInertia(5)
             )
-        )
+        );
         test.ok(
             new Vec3(600, 600, 600).almostEquals(
                 new HollowSphere(3).calculateLocalInertia(100)
             )
-        )
+        );
+        test.done();
     }
 }
