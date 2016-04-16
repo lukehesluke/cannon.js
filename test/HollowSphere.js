ece42dfbcc3600f8c3ecc026c86a10b826eb90e8
Luke Winship <luke.winship@googlemail.com>
2016-04-16 18:33:09 +0100

    Added failing tests for HollowSphere shape

---
+++ test/HollowSphere.js
@@ -0,0 +1,32 @@
+var Vec3 = require("../src/math/Vec3");
+var HollowSphere = require('../src/shapes/HollowSphere');
+
+var almostEqual = function(numA, numB, precision) {
+    if(precision===undefined){
+        precision = 1e-6;
+    }
+    return Math.abs(numA - numB) < precision
+}
+
+module.exports = {
+    volume: function(test) {
+        test.ok(almostEqual(new HollowSphere(10).volume(), 0))
+        test.ok(almostEqual(new HollowSphere(3).volume(), 0))
+    },
+    updateBoundingSphereRadius: function(test) {
+        test.equal(new HollowSphere(10).updateBoundingSphereRadius(), 10)
+        test.equal(new HollowSphere(3).updateBoundingSphereRadius(), 3)
+    },
+    calculateLocalInertia: function(test) {
+        test.ok(
+            new Vec3(333.3333333333333, 333.3333333333333, 333.3333333333333).almostEquals(
+                new HollowSphere(10).calculateLocalInertia(5)
+            )
+        )
+        test.ok(
+            new Vec3(600, 600, 600).almostEquals(
+                new HollowSphere(3).calculateLocalInertia(100)
+            )
+        )
+    }
+}
